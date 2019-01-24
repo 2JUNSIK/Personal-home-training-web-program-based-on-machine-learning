23488551e908d0405333495d7ff0c32a1bf2730b
2junsik <gch01214@gmail.com>
2019-01-24 21:52:59 +0900

    회원정보를 DB에서 삭제하는 컨트롤러

---
+++ com/controller/Delete.java
@@ -0,0 +1,41 @@
+package com.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.model.UserDAO;
+
+/**
+ * Servlet implementation class Delete
+ */
+@WebServlet("/Delete")
+public class Delete extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		String id = session.getAttribute("uid").toString();
+		
+		UserDAO dao = new UserDAO();
+	
+		int cnt = dao.deleteMember(id);
+		
+		PrintWriter out = response.getWriter();
+		if(cnt>0) {
+			out.print("success");
+		}else {
+			out.print("fail");
+		}
+	}
+
+}
